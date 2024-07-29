1fb86d53f73acc5d3ff0ce5fea64f773f8754b68
Rishabh <rishabhgit1704@gmail.comin>
2024-07-29 18:03:50 +0530

    added new files

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./Database/Connection');
+
+
+const authRoutes = require('./controllers/Authmiddleware');
+const protectedRoute = require('./controllers/Protectedroute');
 const UserModel = require('./Model/User');
+
 const Router = require("./Routes/AllRoutes");
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -24,47 +29,11 @@ connectDB();
 UserModel();
 
 
-const users = [];
-// Middleware to check JWT
-const authenticateToken = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) return res.sendStatus(403);
-  jwt.verify(token, 'secretkey', (err, user) => {
-      if (err) return res.sendStatus(403);
-      req.user = user;
-      next();
-  });
-};
-
-// Signup endpoint
-app.post('/sign-up', async (req, res) => {
-  const { username, email, phone, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  users.push({ username, email, phone, password: hashedPassword });
-  res.status(201).send('User created');
-});
-
-// Login endpoint
-app.post('/log-in', async (req, res) => {
-  const { email, password } = req.body;
-  const user = users.find(u => u.email === email);
-  
-  if (user && await bcrypt.compare(password, user.password)) {
-      const token = jwt.sign({ username: user.username, email: user.email }, 'secretkey');
-      res.json({ token });
-  } else {
-      res.status(400).send('Invalid credentials');
-  }
-});
-
-
-
 app.use('/', Router);
 
-app.get('/admindashboard', authenticateToken, (req, res) => {
-  res.json({ message: 'This is your profile', user: req.user });
-});
+app.use('/auth', authRoutes);
+app.use('/protected', protectedRoute);
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
